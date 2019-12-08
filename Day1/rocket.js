5a29233107c1d84e3750743a87617845837779d4
kthays <kthays23@gmail.com>
2019-12-08 14:11:22 -0700

    Added function to sum fuel requirements

--- Day1/rocket.js
+++ Day1/rocket.js
@@ -1,11 +1,12 @@
 module.exports = {
-    GetFuelRequirement,
+    ModuleFuelRequirement,
+    TotalFuelRequirement,
 }
 
 // Fuel required to launch a given module is based on its mass.
 //  Specifically, to find the fuel required for a module, 
 //  take its mass, divide by three, round down, and subtract 2.
-function GetFuelRequirement(moduleMass) {
+function ModuleFuelRequirement(moduleMass) {
     // Validate our input
     if (moduleMass <= 0) throw RangeError('invalid module mass');
 
@@ -16,4 +17,12 @@ function GetFuelRequirement(moduleMass) {
     if (fuel <= 0) throw new RangeError('invalid fuel requirement');
 
     return fuel;
-}
+};
+
+// Returns the total fuel requirements, given an array of all module masses
+function TotalFuelRequirement(moduleMasses) {
+    return moduleMasses.reduce(
+        ( total, curMass ) => total + ModuleFuelRequirement(curMass),
+        0
+    );
+};
